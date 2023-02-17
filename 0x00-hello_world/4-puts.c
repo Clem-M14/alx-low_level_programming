598fb08962719c4614be08c351dfc21b8a33dfb7
My Name <myname@email.com>
2023-02-16 19:13:20 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -Writing a C program that prints a string, followed by a new line
+ * Return:0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+retun(0);
+}
